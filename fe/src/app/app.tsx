3a64df0b634af6115c256df442729ca0062fd308
Dillan Johnson <johnsondillan@gmail.com>
2026-05-30 15:39:34 -0600

    got the initialization set up correctly

--- fe/src/app/app.tsx
+++ fe/src/app/app.tsx
@@ -1,44 +1,54 @@
-import { useEffect, useMemo, useRef } from 'react'
-import { createAppApiAdapter } from './adapter/app-api-adapter'
-import { CsvImportWrapper } from './csv-import/view/wrapper'
-import { AppDomain } from './domain/app-domain'
+import { useEffect, useLayoutEffect, useMemo, useRef, useState } from "react";
+import { createAppApiAdapter } from "./adapter/app-api-adapter";
+import { CsvImportWrapper } from "./csv-import/view/wrapper";
+import { AppDomain } from "./domain/app-domain";
 
 const resolveApiBaseUrl = () => {
-  const envValue = import.meta.env.VITE_API_BASE_URL
+  const envValue = import.meta.env.VITE_API_BASE_URL;
 
-  if (typeof envValue === 'string' && envValue.trim()) {
-    return envValue
+  if (typeof envValue === "string" && envValue.trim()) {
+    return envValue;
   }
 
-  return 'http://127.0.0.1:4000'
-}
+  return "http://127.0.0.1:4000";
+};
+const createApp = () => {
+  console.log("dillan - creating app domain!");
+
+  const api = createAppApiAdapter({
+    apiBaseUrl: resolveApiBaseUrl(),
+  });
+
+  return new AppDomain({
+    api,
+  });
+};
 
 export const App = () => {
-  const disposeTimeoutIdRef = useRef<number | null>(null)
+  const disposeTimeoutIdRef = useRef<number | null>(null);
 
-  const appDomain = useMemo(() => {
-    const api = createAppApiAdapter({
-      apiBaseUrl: resolveApiBaseUrl()
-    })
+  const [appDomain, setAppDomain] = useState(() => {
+    const app = createApp();
+    return app;
+  });
 
-    return new AppDomain({
-      api
-    })
-  }, [])
-
-  useEffect(() => {
-    if (disposeTimeoutIdRef.current !== null) {
-      window.clearTimeout(disposeTimeoutIdRef.current)
-      disposeTimeoutIdRef.current = null
-    }
-
+  useLayoutEffect(() => {
     return () => {
-      disposeTimeoutIdRef.current = window.setTimeout(() => {
-        appDomain.dispose()
-        disposeTimeoutIdRef.current = null
-      }, 0)
-    }
-  }, [appDomain])
+      console.log("dillan - disposing app domain!");
+      appDomain.dispose();
+    };
+    // if (disposeTimeoutIdRef.current !== null) {
+    //   window.clearTimeout(disposeTimeoutIdRef.current)
+    //   disposeTimeoutIdRef.current = null
+    // }
 
-  return <CsvImportWrapper appDomain={appDomain} />
-}
+    // return () => {
+    //   disposeTimeoutIdRef.current = window.setTimeout(() => {
+    //     appDomain.dispose()
+    //     disposeTimeoutIdRef.current = null
+    //   }, 0)
+    // }
+  }, []);
+
+  return <CsvImportWrapper appDomain={appDomain} />;
+};
